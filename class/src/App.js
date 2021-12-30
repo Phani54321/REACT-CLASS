8576c5f8b9df59ca78a66aa959ae25e5f112b8ec
Phani54321 <92142841+Phani54321@users.noreply.github.com>
2021-12-30 23:07:08 +0530

    f

---
+++ class/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Car extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
+}
+
+ReactDOM.render(<Car />, document.getElementById('root'));
+
+export default Car;
+
+export default App;
